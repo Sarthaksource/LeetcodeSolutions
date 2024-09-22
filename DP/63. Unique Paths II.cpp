0bff824eabc997580c188ec96d7a53bc8d9ef124
Sarthaksource <78723977+Sarthaksource@users.noreply.github.com>
2024-09-22 11:12:39 +0530

    Update 63. Unique Paths II.cpp (Space Optimization Done)

--- DP/63. Unique Paths II.cpp
+++ DP/63. Unique Paths II.cpp
@@ -26,7 +26,7 @@ public:
 };
 */
 
-
+/*
 class Solution {
 
 // Tabulation - O(m*n) tc, O(m*n) sc
@@ -59,5 +59,43 @@ public:
         }
         
         return dp[m-1][n-1];
+    }
+};*/
+
+
+class Solution {
+
+// Space Optimized - O(m*n) tc, O(n) sc
+
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if(obstacleGrid[0][0]==1) return 0;
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<int> prev(n, -1);
+
+        for(int i=0; i<m; i++)
+        {
+            vector<int> cur(n, -1);
+            for(int j=0; j<n; j++)
+            {
+                if(i==0 && j==0)
+                {
+                    cur[j] = 1;
+                }
+                else if(i>0 && j>0 && obstacleGrid[i][j]==1) cur[j] = 0;
+                else
+                {
+                    int left = 0, up = 0;
+                    if(i>0 && obstacleGrid[i][j]!=1) up = prev[j];
+                    if(j>0 && obstacleGrid[i][j]!=1) left = cur[j-1];
+
+                    cur[j] = up+left;
+                }
+            }
+            prev = cur;
+        }
+        
+        return prev[n-1];
     }
 };
