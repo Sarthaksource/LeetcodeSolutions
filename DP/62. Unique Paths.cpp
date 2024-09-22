2467795cb75901acb1520edd87ff9c4731d2d78e
Sarthaksource <78723977+Sarthaksource@users.noreply.github.com>
2024-09-22 11:08:47 +0530

    Update 62. Unique Paths.cpp (Space Optimization Done)

--- DP/62. Unique Paths.cpp
+++ DP/62. Unique Paths.cpp
@@ -20,7 +20,7 @@ public:
     }
 };*/
 
-class Solution {
+/*class Solution {
 
 // Tabulation - O(m*n) tc, O(m*n) sc
 
@@ -43,6 +43,32 @@ public:
        }
         return dp[m-1][n-1];
     }
-};
+};*/
 
-// Space Optimized
+
+class Solution {
+
+// Space Optimized - O(m*n) tc, O(n) sc
+
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> prev(n, -1);
+        for(int i=0; i<m; i++)
+        {
+            vector<int> cur(n, -1);
+            for(int j=0; j<n; j++)
+            {
+                if(i==0 || j==0) cur[j] = 1;
+                else
+                {
+                    int left = 0, up = 0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = cur[j-1];
+                    cur[j] = up+left;
+                }
+            }
+            prev = cur;
+        }
+        return prev[n-1];
+    }
+};
